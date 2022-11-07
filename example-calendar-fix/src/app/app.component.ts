49484cdb9fd3d84e1fe7fe305064365e9b2dfd96
Andreu6 <suarezbellavistaandreu@gmail.com>
2022-11-07 19:52:39 +0100

    Calendario

---
+++ example-calendar-fix/src/app/app.component.ts
@@ -0,0 +1,51 @@
+import { Component, OnInit } from '@angular/core';
+import * as moment from 'moment'
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+
+  semana: any = [
+    "Lunes",
+    "Martes",
+    "Miercoles",
+    "Jueves",
+    "Viernes",
+    "Sabado",
+    "Domingo"
+  ];
+
+
+  seleccionarMes: any[];
+  seleccionarDia: any;
+
+  ngOnInit(): void {
+    this.getDaysFromDate(11, 2022)
+  }
+
+  getDaysFromDate(month, year) {
+
+    const startDate = moment.utc(`${year}/${month}/01`)
+    const endDate = startDate.clone().endOf('month')
+    this.seleccionarDia = startDate;
+
+    const diffDays = endDate.diff(startDate, 'days', true)
+    const numberDays = Math.round(diffDays);
+
+    const arrayDays = Object.keys([...Array(numberDays)]).map((a: any) => {
+      a = parseInt(a) + 1;
+      const dayObject = moment(`${year}-${month}-${a}`);
+      return {
+        name: dayObject.format("dddd"),
+        value: a,
+        indexWeek: dayObject.isoWeekday()
+      };
+    });
+
+    this.seleccionarMes = arrayDays;
+  }
+
+}
