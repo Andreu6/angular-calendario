661898633313471215b43a231bfec49f64ea4f56
Andreu6 <suarezbellavistaandreu@gmail.com>
2022-11-08 19:50:44 +0100

    Calendaris

--- example-calendar-fix/src/app/app.component.ts
+++ example-calendar-fix/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import * as moment from 'moment'
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -20,10 +21,13 @@ export class AppComponent implements OnInit {
 
 
   seleccionarMes: any[];
+  seleccionarMes2: any[];
   seleccionarDia: any;
+  seleccionarDia2: any;
 
   ngOnInit(): void {
-    this.getDaysFromDate(11, 2022)
+    this.getDaysFromDate(9, 2022)
+    this.getDaysFromDate2(10,2022)
   }
 
   getDaysFromDate(month, year) {
@@ -46,6 +50,28 @@ export class AppComponent implements OnInit {
     });
 
     this.seleccionarMes = arrayDays;
+  }
+
+  getDaysFromDate2(month2, year2) {
+
+    const startDate2 = moment.utc(`${year2}/${month2}/01`)
+    const endDate2 = startDate2.clone().endOf('month')
+    this.seleccionarDia2 = startDate2;
+
+    const diffDays2 = endDate2.diff(startDate2, 'days', true)
+    const numberDays2 = Math.round(diffDays2);
+
+    const arrayDays2 = Object.keys([...Array(numberDays2)]).map((b: any) => {
+      b = parseInt(b) + 1;
+      const dayObject2 = moment(`${year2}-${month2}-${b}`);
+      return {
+        name: dayObject2.format("dddd"),
+        value2: b,
+        indexWeek2: dayObject2.isoWeekday()
+      };
+    });
+
+    this.seleccionarMes2 = arrayDays2;
   }
 
 }
